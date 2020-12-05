4fb1c4fc7581d84b9e43dc86effec204ce31d2f5
Andrew Yatsura <andrewyazura203@gmail.com>
2020-12-05 14:51:55 +0200

    Update day 5 solution

--- day-5/main.py
+++ day-5/main.py
@@ -4,119 +4,114 @@ with open('day-5/input.txt', 'r') as file:
     puzzle_input = [int(i.strip()) for i in file.read().split(',')]
 
 
+class InputException(Exception):
+    pass
+
+
 class IntcodeInterpreter:
     def __init__(self, memory):
-        self.memory = memory
-        self.preset_input = []
-        self.saved_output = []
+        self.running = True
+        self.memory = memory.copy()
         self.pointer = 0
+
+        self.saved_input = None
+        self.saved_output = []
+
         self.opcodes = {
             1: {
-                'func': self.sum,
+                'func': self._add,
                 'shift': 4
             },
             2: {
-                'func': self.multiply,
+                'func': self._multiply,
                 'shift': 4
             },
             3: {
-                'func': self.input,
+                'func': self._get_input,
                 'shift': 2
             },
             4: {
-                'func': self.output,
+                'func': self._send_output,
                 'shift': 2
             },
             5: {
-                'func': self.jump_if_true,
+                'func': self._jump_if_true,
                 'shift': 3
             },
             6: {
-                'func': self.jump_if_false,
+                'func': self._jump_if_false,
                 'shift': 3
             },
             7: {
-                'func': self.less_than,
+                'func': self._less_than,
                 'shift': 4
             },
             8: {
-                'func': self.equals,
+                'func': self._equals,
                 'shift': 4
             },
+            99: {
+                'func': self._terminate,
+                'shift': 0
+            }
         }
 
-    def provide_input(self, *args):
-        self.preset_input.extend(args)
-
-    def interpret_instruction(self, instruction):
-        instruction = str(instruction).rjust(5, ' ')
-        opcode = int(instruction[3:])
-        modes = [int(x) if x != ' ' else 0 for x in instruction[2::-1]]
-
-        return opcode, modes
-
-    def sum(self, addr1, addr2, addr3):
+    def _add(self, addr1, addr2, addr3):
         self.memory[addr3] = self.memory[addr1] + self.memory[addr2]
-        return self.memory[addr3]
+        self.pointer += 4
 
-    def multiply(self, addr1, addr2, addr3):
+    def _multiply(self, addr1, addr2, addr3):
         self.memory[addr3] = self.memory[addr1] * self.memory[addr2]
-        return self.memory[addr3]
+        self.pointer += 4
 
-    def input(self, addr):
-        self.memory[addr] = self.preset_input.pop(0) \
-            if self.preset_input else self._input()
-        return self.memory[addr]
+    def _get_input(self, addr1):
+        if self.saved_input is not None:
+            self.memory[addr1] = self.saved_input
+            self.saved_input = None
 
-    def _input(self):
-        answer = None
+        else:
+            raise InputException(
+                f'self.saved_input is empty; pointer={self.pointer}')
 
-        while answer is None:
-            try:
-                answer = int(input('enter value: '))
-            except:
-                answer = None
+        self.pointer += 2
 
-        return answer
+    def _send_output(self, addr1):
+        self.saved_output.append(self.memory[addr1])
+        self.pointer += 2
 
-    def output(self, addr):
-        r = self.memory[addr]
-        self.saved_output.append(r)
-        return r
-
-    def jump_if_true(self, addr1, addr2):
+    def _jump_if_true(self, addr1, addr2):
         if self.memory[addr1] != 0:
             self.pointer = self.memory[addr2]
-            return
 
-        return self.pointer
+        else:
+            self.pointer += 3
 
-    def jump_if_false(self, addr1, addr2):
+    def _jump_if_false(self, addr1, addr2):
         if self.memory[addr1] == 0:
             self.pointer = self.memory[addr2]
-            return
 
-        return self.pointer
+        else:
+            self.pointer += 3
 
-    def less_than(self, addr1, addr2, addr3):
+    def _less_than(self, addr1, addr2, addr3):
         result = int(self.memory[addr1] < self.memory[addr2])
         self.memory[addr3] = result
-        return result
+        self.pointer += 4
 
-    def equals(self, addr1, addr2, addr3):
+    def _equals(self, addr1, addr2, addr3):
         result = int(self.memory[addr1] == self.memory[addr2])
         self.memory[addr3] = result
-        return result
+        self.pointer += 4
+
+    def _terminate(self):
+        self.running = False
 
     def execute(self):
-        while self.pointer <= len(self.memory):
+        while self.running and self.pointer <= len(self.memory):
             instruction = self.memory[self.pointer]
             opcode, modes = self.interpret_instruction(
                 instruction
             )
-
-            if opcode == 99:
-                return self.memory, self.saved_output
 
             opcode_func = self.opcodes[opcode]['func']
             opcode_shift = self.opcodes[opcode]['shift']
@@ -129,22 +124,33 @@ class IntcodeInterpreter:
                 elif modes[i - 1] == 1:
                     params.append(self.pointer + i)
 
-            result = opcode_func(*params)
-            self.pointer += opcode_shift if result is not None else 0
+            opcode_func(*params)
+
+    def interpret_instruction(self, instruction):
+        instruction = str(instruction).rjust(5, ' ')
+        opcode = int(instruction[3:])
+        modes = [int(x) if x != ' ' else 0 for x in instruction[2::-1]]
+
+        return opcode, modes
+
+    def set_input(self, value):
+        self.saved_input = value
 
 
 def part_1(puzzle_input):
     puzzle = puzzle_input.copy()
     interpreter = IntcodeInterpreter(puzzle)
-    interpreter.provide_input(1)
-    return interpreter.execute()[1][-1]
+    interpreter.set_input(1)
+    interpreter.execute()
+    return interpreter.saved_output[-1]
 
 
 def part_2(puzzle_input):
     puzzle = puzzle_input.copy()
     interpreter = IntcodeInterpreter(puzzle)
-    interpreter.provide_input(5)
-    return interpreter.execute()[1][-1]
+    interpreter.set_input(5)
+    interpreter.execute()
+    return interpreter.saved_output[-1]
 
 
 print(part_1(puzzle_input))
